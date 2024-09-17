80e8b58bbee6e6f0f9bf7e1d28329c12615aecbe
EC2 Default User <devops@intuitivo.com>
2024-09-17 23:46:09 +0000

    Terranator updates

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Run tests"
+TYPE="$1"
+
+case $TYPE in
+default)
+  ./scripts/requirements.sh
+  echo "Run tests"
+  pytest --cov .
+  chown -vR 1000:1000 .
+  ;;
+terraform)
+  echo "Run tests"
+  terraform test
+  ;;
+*)
+  echo "Run tests"
+  ;;
+esac
