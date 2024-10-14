91dbd3829e4a954aedbdadd0f09210a70f754485
EC2 Default User <devops@intuitivo.com>
2024-10-14 16:25:19 +0000

    Terranator updates

--- scripts/release_notes.sh
+++ scripts/release_notes.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-sudo yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo yum install gh -y
-
 latest_release_tag=$(gh release view --json tagName --jq '.tagName')
 if [ -z "$latest_release_tag" ]; then
   echo "No se pudo obtener el último release."
@@ -15,4 +12,4 @@ gh release create \
   release/$GITHUB_RUN_NUMBER \
   -t v$GITHUB_RUN_NUMBER.$GITHUB_RUN_ATTEMPT \
   --generate-notes \
-  --notes-start-tag $latest_release_tag
+  --notes-start-tag $latest_release_tag #--prerelease
