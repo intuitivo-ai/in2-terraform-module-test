644528f40d755e3b5836cc7a4376c0fefb93dcf3
EC2 Default User <devops@intuitivo.com>
2024-10-14 20:00:38 +0000

    Terranator updates

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -5,4 +5,6 @@ source $DIR/functions.sh
 
 cd $DIR
 echo "Install requirements"
+sudo yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo yum install gh -y
 install_pip_requirements
